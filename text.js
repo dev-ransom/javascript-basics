cf023fbcc751d5ece4eb6cb05ac720afb7e714bf
idungstanley <sundaystanley56@gmail.com>
2022-10-23 15:57:21 +0100

    add links

--- text.js
+++ text.js
@@ -1,21 +1,23 @@
-for (i = 0; i < 10; i++) {
+var links = document.getElementsByTagName('a')
+let sunday = [
+  'stanley',
+  'daniel',
+  'ransome',
+  'destiny',
+  'gideon',
+  'george',
+  'benjamin',
+]
+let div_fam = document.querySelector('.family')
+let parent_div = document.querySelector('.parent')
+console.log(div_fam)
 
-
- if (i == 5 || i == 3) {
-
-  continue;
-
- }
-
-console.log(i);
-
-if (i == 7) {
-
- break;
- 
+for (let i = 0; i < sunday.length; i++) {
+ let para = document.createElement("p")
+ parent_div. append(para)
+para.innerHTML = sunday[i]
 }
 
-
+for (let i = 0; i < links.length; i++) {
+  var class_link = (links[i].className = 'link-' + i)
 }
-
-console.log("I have broken out of the length")
